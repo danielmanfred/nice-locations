8f7679a66e3a268c9f5513110431c1549de78db8
Daniel.souza <daniel.souza@MacBook-Pro-de-mendelics.local>
2020-04-19 21:07:00 -0300

    HBS testing e2e

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 import { resolve } from 'path';
 
 async function bootstrap() {
-  const APP_DIRECTORY = resolve(__dirname, );
+  const APP_DIRECTORY = resolve(__dirname, '..');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+  
   await app.listen(3000);
 }
 bootstrap();
